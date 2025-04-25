831d973b60d2169ef3f41c8f28b1eb949b182428
M.Sufiyan Fahim <156201756+Sufiyan-Sakaria@users.noreply.github.com>
2025-04-25 21:20:16 +0500

    Delete Server directory

--- Server/src/routes/AuthRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { Login } from "../controllers/AuthController";
-
-const router = Router();
-
-// Login route
-router.post("/login", Login);
-
-export default router;
